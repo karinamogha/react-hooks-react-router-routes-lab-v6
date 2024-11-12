145b52c5c8795a26d00670c09c03fb6a12031dce
Karina Mogha <moghakarina@gmail.com>
2024-11-12 12:28:40 -0500

    work

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -4,18 +4,13 @@ import NavBar from '../components/NavBar';
 const Directors = () => {
   const [directors, setDirectors] = useState([]);
 
-  useEffect(() => {
-    const fetchDirectors = async () => {
-      try {
-        const response = await fetch("http://localhost:4000/directors");
-        const data = await response.json();
-        setDirectors(data);
-      } catch (error) {
-        console.error("Failed to fetch directors:", error);
-      }
-    };
-
-    fetchDirectors();
+  useEffect(() =>{
+    fetch(`http://localhost:4000/actors`)
+    .then(r => r.json())
+    .then(data => {
+      setDirectors(data);
+  })
+    .catch(error => console.error(error));
   }, []); 
 
   return (
